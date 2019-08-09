45f66d387726f4797f37fa7b3270f5ada3c76545
Greg Ives <gregives774@gmail.com>
2019-08-09 21:41:57 +0100

    Remove Tailwind CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,9 +33,7 @@ export default {
    */
   devModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
--- pages/index.vue
+++ pages/index.vue
@@ -35,11 +35,6 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
 .container {
   margin: 0 auto;
   min-height: 100vh;
