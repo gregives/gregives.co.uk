0bf08501e34333fec251f31962ff5b3c5db532f7
Greg Ives <gregives774@gmail.com>
2019-08-10 21:33:03 +0100

    Add support for sass

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="container">
+      Greg Ives
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Experience</li>
+          <li>About</li>
+          <li>Contact me</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+header
+  clear: both
+
+nav
+  float: right
+
+  li
+    display: inline-block
+</style>
