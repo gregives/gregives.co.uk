9d91f6c95f24aed01a09a415acdc9ee4fe1f642a
Greg Ives <gregives774@gmail.com>
2019-08-13 23:06:13 +0100

    Test larger font-size

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,10 @@
           I am
           <typer />
         </h1>
-        <div class="description">
-          Hi there! I'm a final-year Computer Science student at the
-          <a href="https://sheffield.ac.uk">University of Sheffield</a>. I'm
-          looking for an opportunity in software development from June&nbsp;2020
-          &mdash; check out my experience below or contact&nbsp;me!
-        </div>
+        Hi there! I'm a final-year Computer Science student at the
+        <a href="https://sheffield.ac.uk">University of Sheffield</a>. I'm
+        looking for an opportunity in software development from June&nbsp;2020
+        &mdash; check out my experience below or contact&nbsp;me!
       </div>
     </div>
   </div>
@@ -26,9 +24,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-.description {
-  font-size: 120%;
-}
-</style>
