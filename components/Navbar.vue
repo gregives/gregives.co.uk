8f32b0feb9776ec9e6eddddd252b691d37c0b96e
Greg Ives <gregives774@gmail.com>
2019-08-13 21:21:15 +0100

    Start work on hamburger menu icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,9 +21,22 @@
           </li>
         </ol>
       </nav>
+      <div id="hamburger" :class="{ open }" @click="open = !open">
+        <div></div>
+      </div>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '~/assets/sass/_variables';
@@ -39,11 +52,16 @@ header {
   #logo {
     display: inline-block;
     font-family: $header-font;
-    font-size: 115%;
+    font-size: 120%;
   }
 
   nav {
+    display: none;
     float: right;
+
+    @media (min-width: $break-lg) {
+      display: block;
+    }
 
     ol {
       counter-reset: navbar-links;
@@ -66,6 +84,63 @@ header {
           right: calc(100% + 1vmin);
           top: 0.5vmin;
         }
+      }
+    }
+  }
+
+  #hamburger {
+    height: 100%;
+    padding: 10px;
+    position: absolute;
+    right: 0;
+    top: 0;
+
+    @media (min-width: $break-lg) {
+      display: none;
+    }
+
+    > div {
+      background-color: transparentize($color: black, $amount: 0.1);
+      border-radius: 1px;
+      height: 2px;
+      position: relative;
+      right: 0;
+      top: calc(50% - 1px);
+      transition: transform 300ms ease, background-color 300ms ease;
+      width: 28px;
+
+      &::before,
+      &::after {
+        background-color: transparentize($color: black, $amount: 0.1);
+        border-radius: 1px;
+        content: '';
+        height: 100%;
+        position: absolute;
+        width: 100%;
+      }
+
+      &::before {
+        top: -7px;
+        transition: top 300ms ease;
+      }
+
+      &::after {
+        bottom: -7px;
+        transition: bottom 300ms ease, transform 300ms ease;
+      }
+    }
+
+    &.open > div {
+      background-color: transparent;
+      transform: rotate(45deg);
+
+      &::before {
+        top: 0;
+      }
+
+      &::after {
+        bottom: 0;
+        transform: rotate(90deg);
       }
     }
   }
