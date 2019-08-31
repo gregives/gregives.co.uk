4f4668f3d93a17ff7a05dee51c5f0455a52c240b
Greg Ives <gregives774@gmail.com>
2019-08-31 17:43:54 +0100

    Remove scoped CSS from shapes

--- components/Shapes.vue
+++ components/Shapes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shapes">
+  <div class="shapes shapes-full">
     <div data-tilt>
       <div class="triangle"></div>
       <div class="circle"></div>
@@ -7,47 +7,6 @@
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.shapes {
-  height: 100%;
-  left: 0;
-  overflow: hidden;
-  position: absolute;
-  top: 0;
-  width: 100%;
-
-  > div {
-    height: 100%;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100%;
-
-    div {
-      position: absolute;
-    }
-
-    :nth-child(1) {
-      left: calc(20% - 50px);
-      top: 20%;
-      transform: rotate(20deg) translate(-50%, -50%);
-    }
-
-    :nth-child(2) {
-      left: 50%;
-      top: 25%;
-      transform: scale(0.6) translate(-50%, -50%);
-    }
-
-    :nth-child(3) {
-      left: calc(60% + 100px);
-      top: 10%;
-      transform: rotate(60deg) scale(0.8) translate(-50%, -50%);
-    }
-  }
-}
-</style>
 
 <style lang="scss">
 .shapes {
@@ -88,6 +47,45 @@
       position: absolute;
       top: 0;
       width: 150%;
+    }
+  }
+}
+
+.shapes-full {
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+
+  > div {
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+
+    div {
+      position: absolute;
+    }
+
+    :nth-child(1) {
+      left: calc(20% - 50px);
+      top: 20%;
+      transform: rotate(20deg) translate(-50%, -50%);
+    }
+
+    :nth-child(2) {
+      left: 50%;
+      top: 25%;
+      transform: scale(0.6) translate(-50%, -50%);
+    }
+
+    :nth-child(3) {
+      left: calc(60% + 100px);
+      top: 10%;
+      transform: rotate(60deg) scale(0.8) translate(-50%, -50%);
     }
   }
 }
