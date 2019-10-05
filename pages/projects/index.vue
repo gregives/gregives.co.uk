f2c74aa89245bf9ec1de05b211def2cdf9e856ad
Greg Ives <gregives774@gmail.com>
2019-10-05 22:17:52 +0100

    Update project list

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,42 +3,29 @@
     <h1 class="projects__title">
       My <span class="projects__title--primary">Projects</span>
     </h1>
-    <p class="projects__introduction">
-      Here’s a selection of projects which I’ve created since I became
-      interested in coding... I’ve made lots of things from Android apps to
-      websites to a TV remote, have a browse!
-    </p>
     <ol class="projects__list">
       <li
         v-for="project in projects"
         :key="project.title"
         class="projects__list-item"
-        :class="{ 'projects__list-item--small': !project.link }"
       >
-        <nuxt-link
-          v-if="project.link"
-          :to="project.link"
-          class="projects__list-link"
-        >
+        <nuxt-link :to="project.link" class="projects__list-link">
           <div class="projects__list-image">
             <lazy-image src="placeholder.png" :alt="project.title" />
           </div>
-          <h3 class="projects__list-title">
-            {{ project.titleShort || project.title }}
-          </h3>
-          <small class="projects__list-date">
-            {{ project.date.getFullYear() }}
-          </small>
-          <p
-            class="projects__list-description"
-            v-html="project.description"
-          ></p>
+          <div class="projects__list-content">
+            <h4 class="projects__list-title">
+              {{ project.titleShort || project.title }}
+            </h4>
+            <small class="projects__list-date">
+              {{ project.date.getFullYear() }}
+            </small>
+            <p
+              class="projects__list-description"
+              v-html="project.description"
+            ></p>
+          </div>
         </nuxt-link>
-        <p
-          v-else
-          class="projects__list-description"
-          v-html="project.description"
-        ></p>
       </li>
     </ol>
   </main>
@@ -63,13 +50,6 @@ export default {
     )
 
     projects.sort((projectA, projectB) => projectB.date - projectA.date)
-
-    // Add other content into list of projects
-    projects.splice(3, 0, {
-      description:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione minima quasi porro voluptatibus nesciunt assumenda voluptatem dolor illo libero totam! Repellat sequi quibusdam architecto aliquam rerum totam ipsum veritatis ex.'
-    })
-
     return {
       projects
     }
@@ -84,6 +64,7 @@ export default {
 
 .projects__title {
   @include title;
+  transition: color 150ms ease-out;
 }
 
 .projects__title--primary {
@@ -92,7 +73,6 @@ export default {
 
 .projects__list {
   display: grid;
-  grid-auto-rows: auto;
   grid-template-columns: 100%;
   grid-gap: calc(1rem + 5vmin);
 
@@ -101,43 +81,14 @@ export default {
   }
 }
 
-.projects__list-item {
-  grid-row: span 2;
-  position: relative;
-
-  &::before {
-    background-color: $color--body-overlay;
-    border-radius: $border-radius;
-    box-shadow: $box-shadow;
-    content: '';
-    height: 80%;
-    left: 0;
-    position: absolute;
-    top: 20%;
-    width: 100%;
-    z-index: -1;
-  }
-}
-
-.projects__list-item--small {
-  grid-row: span 1;
-
-  &::before {
-    height: 100%;
-    top: 0;
-  }
-
-  .projects__list-description {
-    padding: 1.5rem;
-  }
-}
-
 .projects__list-link {
   display: block;
-  padding: 1.5rem;
-  padding-top: 0;
 
   &:hover {
+    .projects__list-title {
+      color: $color--primary;
+    }
+
     .projects__list-image {
       filter: saturate(0.5) brightness(1.2);
 
@@ -154,25 +105,28 @@ export default {
 
 .projects__list-title {
   display: inline-block;
+  margin-bottom: 1rem;
 }
 
 .projects__list-date {
   color: $color--text-muted;
   font-family: $font--fancy;
   font-size: 90%;
-  margin-left: 1rem;
+  transition: color 150ms ease-out;
+
+  &::before {
+    content: '\2014';
+  }
 }
 
 .projects__list-image {
   border-radius: $border-radius;
-  border-bottom-left-radius: 0;
-  margin-bottom: 1rem;
-  margin-left: -1.5rem;
+  margin-right: 1.5rem;
   overflow: hidden;
-  padding-bottom: 50%;
+  padding-bottom: calc(50% - 0.75rem);
   position: relative;
   transition: filter 300ms ease;
-  width: 100%;
+  width: calc(100% - 1.5rem);
 
   &::after {
     background-color: $color--primary;
@@ -196,5 +150,15 @@ export default {
     transition: transform 300ms ease;
     width: 100%;
   }
+}
+
+.projects__list-content {
+  background-color: $color--body-overlay;
+  border-radius: $border-radius;
+  box-shadow: $box-shadow;
+  margin-top: -1.5rem;
+  margin-left: 1.5rem;
+  padding: 1.5rem;
+  position: relative;
 }
 </style>
