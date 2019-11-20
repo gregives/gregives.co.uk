73e29ec89775dd3a85ad3d936cde93988c17fa95
Greg Ives <gregives774@gmail.com>
2019-11-20 16:17:55 +0000

    Change html-minifier options

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,7 +108,22 @@ export default {
   // Build configuration
   build: {
     html: {
-      minify: {}
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true
+      }
     },
     extend(
       config,
