5b1e1e35aa1dc34059b66e850baa573789d05eb2
Greg Ives <gregives774@gmail.com>
2019-11-21 00:30:52 +0000

    Scroll page to route hash

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,6 +49,19 @@ export default {
     window.addEventListener('beforeinstallprompt', (event) => {
       event.preventDefault()
     })
+
+    // Check if the page should scroll to hash
+    const hash = this.$route.hash
+    if (hash) {
+      this.scrollToHash(hash)
+    }
+  },
+  methods: {
+    scrollToHash(hash) {
+      this.$nextTick(() => {
+        location.href = hash
+      })
+    }
   }
 }
 </script>
