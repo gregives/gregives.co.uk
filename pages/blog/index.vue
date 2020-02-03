8cc6901baa11ff7dd24db77205c73b0ee056b804
Greg Ives <gregives774@gmail.com>
2020-02-03 20:50:35 +0000

    Move post and project cards into components

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,33 +3,20 @@
     <h1 class="blog__title">
       My <span class="blog__title--primary">Blog</span> Posts
     </h1>
-    <ol class="blog__list">
-      <li v-for="post in posts" :key="post.title" class="blog__post">
-        <h3>
-          <nuxt-link :to="post.link" class="blog__post-link">
-            {{ post.title }}
-          </nuxt-link>
-        </h3>
-        <p class="blog__post-details">
-          {{
-            post.date.toLocaleString('en-US', {
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric'
-            })
-          }}
-          &mdash; {{ post.mins }} minute read
-        </p>
-        <p class="blog__post-description">{{ post.description }}</p>
-      </li>
+    <ol class="blog__posts">
+      <post-card v-for="post in posts" :key="post.title" :post="post" />
     </ol>
   </main>
 </template>
 
 <script>
+import PostCard from '~/components/PostCard'
 import { postLoader, postSlugs } from '~/contents/blog'
 
 export default {
+  components: {
+    PostCard
+  },
   head() {
     return {
       title: 'Blog'
@@ -68,71 +55,8 @@ export default {
   @include title--primary;
 }
 
-.blog__list {
+.blog__posts {
   display: grid;
   grid-gap: 1.5rem;
-}
-
-.blog__post {
-  background: linear-gradient(to right, $color__body, transparent),
-    linear-gradient(transparent 75%, #{transparentize(black, 0.95)}),
-    $color__body--overlay;
-  border-radius: $border-radius;
-  padding: 1.5rem;
-  padding-top: 1rem;
-  padding-left: 0;
-  position: relative;
-
-  &::before {
-    box-shadow: $box-shadow--small;
-    border: $border-weight solid $color__primary--muted;
-    border-radius: $border-radius;
-    content: '';
-    height: calc(100% + #{2 * $border-weight});
-    left: 0;
-    position: absolute;
-    top: -$border-weight;
-    transition: box-shadow 150ms ease-out, border-color 150ms ease-out;
-    width: calc(100% + #{$border-weight});
-    z-index: -1;
-  }
-
-  &::after {
-    background: linear-gradient(to right, $color__body 2rem, transparent);
-    content: '';
-    height: calc(100% + 2rem);
-    left: -2rem;
-    position: absolute;
-    top: -1rem;
-    width: calc(100% + 2rem);
-    z-index: -1;
-  }
-
-  > h3 {
-    margin-bottom: 0.5rem;
-  }
-
-  &:hover::before {
-    border-color: $color__primary;
-    box-shadow: $box-shadow;
-  }
-}
-
-.blog__post-link {
-  @include link;
-
-  &::after {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    content: '';
-    top: 0;
-  }
-}
-
-.blog__post-details {
-  color: $color__text--muted;
-  margin-bottom: 0.5rem;
 }
 </style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,45 +3,24 @@
     <h1 class="projects__title">
       My <span class="projects__title--primary">Projects</span>
     </h1>
-    <ol class="projects__list">
-      <li
+    <ol class="projects__projects">
+      <project-card
         v-for="project in projects"
         :key="project.title"
-        class="projects__list-item"
-      >
-        <nuxt-link :to="project.link" class="projects__list-link">
-          <div
-            :data-title="project.titleShort || project.title"
-            :style="
-              `--scroll-speed: ${(project.titleShort || project.title).length /
-                1.5}s`
-            "
-            class="projects__list-image"
-          >
-            <lazy-image :src="project.image" :alt="project.title" />
-          </div>
-          <div class="projects__list-content">
-            <h3 class="projects__list-title">
-              {{ project.titleShort || project.title }}
-            </h3>
-            <small class="projects__list-date">
-              {{ project.date.getFullYear() }}
-            </small>
-            <p
-              v-html="project.description"
-              class="projects__list-description"
-            ></p>
-          </div>
-        </nuxt-link>
-      </li>
+        :project="project"
+      />
     </ol>
   </main>
 </template>
 
 <script>
+import ProjectCard from '~/components/ProjectCard'
 import { projectLoader, projectSlugs } from '~/contents/projects'
 
 export default {
+  components: {
+    ProjectCard
+  },
   head() {
     return {
       title: 'Projects'
@@ -94,7 +73,7 @@ export default {
   @include title--primary;
 }
 
-.projects__list {
+.projects__projects {
   display: grid;
   grid-template-columns: 100%;
   grid-gap: 3rem;
@@ -102,136 +81,5 @@ export default {
   @media (min-width: $breakpoint--md) {
     grid-template-columns: 1fr 1fr;
   }
-}
-
-.projects__list-link {
-  display: block;
-
-  &:hover {
-    .projects__list-content {
-      border-color: $color__primary;
-      box-shadow: $box-shadow;
-    }
-
-    .projects__list-title {
-      color: $color__primary;
-    }
-
-    .projects__list-image {
-      &::before {
-        animation-play-state: running;
-        opacity: 0.3;
-      }
-
-      &::after {
-        opacity: 1;
-      }
-
-      .lazy {
-        filter: grayscale(0.8);
-      }
-    }
-  }
-}
-
-.projects__list-title {
-  display: inline-block;
-  margin-bottom: 1rem;
-}
-
-.projects__list-date {
-  color: $color__text--muted;
-  font-family: $font__fancy;
-  font-size: 90%;
-
-  &::before {
-    content: '\2014';
-  }
-}
-
-.projects__list-image {
-  background-color: $color__body;
-  border-radius: $border-radius;
-  box-shadow: $box-shadow;
-  margin-right: 1.5rem;
-  overflow: hidden;
-  padding-bottom: calc(50% - 0.75rem);
-  position: relative;
-  width: calc(100% - 1.5rem);
-
-  &::before {
-    animation: 10s infinite linear scroll;
-    animation-duration: var(--scroll-speed);
-    animation-play-state: paused;
-    bottom: -25%;
-    color: $color__body--overlay;
-    content: '\00A0\00A0'attr(data-title) '\00A0\00A0'attr(data-title)
-      '\00A0\00A0'attr(data-title);
-    font-family: $font__fancy;
-    font-size: 10em;
-    left: 0;
-    opacity: 0;
-    position: absolute;
-    transition: opacity 300ms ease;
-    white-space: nowrap;
-    z-index: 1;
-  }
-
-  @keyframes scroll {
-    from {
-      transform: translateX(#{-1/3 * 100%});
-    }
-
-    to {
-      transform: translateX(#{-2/3 * 100%});
-    }
-  }
-
-  &::after {
-    background-color: $color__primary--muted;
-    background-image: linear-gradient(transparent, $color__primary);
-    content: '';
-    height: 100%;
-    left: 0;
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    transition: opacity 300ms ease;
-    width: 100%;
-  }
-
-  .lazy {
-    bottom: 0;
-    height: 100%;
-    left: 0;
-    position: absolute;
-    top: 0;
-    transition: filter 300ms ease-out;
-    width: 100%;
-
-    picture {
-      height: 100%;
-
-      .lazy__image {
-        height: 100%;
-        object-fit: cover;
-        object-position: top left;
-      }
-    }
-  }
-}
-
-.projects__list-content {
-  background: linear-gradient(transparent 75%, #{transparentize(black, 0.95)}),
-    $color__body--overlay;
-  border: $border-weight solid $color__primary--muted;
-  border-radius: $border-radius;
-  box-shadow: $box-shadow--small;
-  margin-top: -1.5rem;
-  margin-left: 1.5rem;
-  padding: 1.5rem;
-  position: relative;
-  transition: box-shadow 150ms ease-out, border-color 150ms ease-out;
-  z-index: 1;
 }
 </style>
