d42dcbd7fb224c929468231a2afee6e6da4b560c
Greg Ives <gregives774@gmail.com>
2020-02-18 00:39:05 +0000

    Avoid loading lazy images before intersection

--- plugins/lazysizes.client.js
+++ plugins/lazysizes.client.js
@@ -4,5 +4,5 @@ lazySizes.cfg.lazyClass = 'lazy__image--load'
 lazySizes.cfg.loadingClass = 'lazy__image--loading'
 lazySizes.cfg.loadedClass = 'lazy__image--loaded'
 lazySizes.cfg.preloadClass = 'lazy__image--preload'
-lazySizes.cfg.preloadAfterLoad = true
+
 export default lazySizes
