db1eae2f61c0d65561eabf4fd60b7d48bc20eee5
Greg Ives <gregives774@gmail.com>
2021-05-09 14:33:09 +0100

    Add Open Graph images to Git

--- config/opengraph.js
+++ config/opengraph.js
@@ -1,35 +1,38 @@
+import fs from 'fs'
+import path from 'path'
 import https from 'https'
-import { join } from 'path'
-import { createWriteStream, mkdirSync } from 'fs'
 import dotenv from 'dotenv'
 import routes from '../contents/routes'
 
-// Load .env file
-dotenv.config()
-
-// Generate Open Graph images from script
+// Generate images on `yarn opengraph`
 if (require.main === module) {
   generateImages(routes)
 }
 
 function generateImages(routes) {
-  const API_KEY = process.env.APIFLASH_KEY
+  // Load API key
+  dotenv.config()
+  const { APIFLASH_KEY: API_KEY } = process.env
 
   if (API_KEY !== undefined) {
     ;[...routes, '/'].forEach((route) => {
-      // Create new file for Open Graph image
-      const path = join(process.cwd(), 'static', route.slice(1))
-      mkdirSync(path, { recursive: true })
-      const file = createWriteStream(join(path, 'og.png'))
+      // Create new static directory for Open Graph image
+      const directory = path.join(process.cwd(), 'static', route.slice(1))
+      fs.mkdirSync(directory, { recursive: true })
+
+      // Check if Open Graph image already exists
+      const filepath = path.join(directory, 'og.png')
+      if (fs.existsSync(filepath)) return
 
       // URL to corresponding meta page
       const url = encodeURIComponent(`https://gregives.co.uk/meta${route}`)
 
       // ApiFlash endpoint
-      const api = `https://api.apiflash.com/v1/urltoimage?access_key=${API_KEY}&format=png&height=630&response_type=image&ttl=2592000&url=${url}&width=1200`
+      const api = `https://api.apiflash.com/v1/urltoimage?access_key=${API_KEY}&format=png&height=630&response_type=image&url=${url}&width=1200&fresh=true`
 
-      // Write response to stream
+      // Write response to file
       https.get(api, (response) => {
+        const file = fs.createWriteStream(filepath)
         response.pipe(file)
         file.on('finish', file.close)
       })
