e26893107d38cf2b13196f53ad2472c972a83683
Greg Ives <greg@gregives.co.uk>
2025-06-13 13:08:05 +0100

    Improve badge on mobile

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -6,12 +6,12 @@ export function Badge({ className, children, ...properties }: BadgeProperties) {
   return (
     <div
       className={twMerge(
-        "absolute -inset-0 overflow-hidden pointer-events-none flex justify-end items-start",
+        "absolute inset-0 overflow-hidden pointer-events-none flex justify-end items-start",
         className
       )}
       {...properties}
     >
-      <div className="origin-top-left rotate-[30deg] p-2 before:absolute before:bg-yellow-200 dark:before:bg-slate-900 before:inset-0 before:-mx-64 before:-z-10 before:shadow">
+      <div className="origin-top-left rotate-[30deg] p-1 md:p-2 before:absolute before:bg-yellow-200 dark:before:bg-slate-900 before:inset-0 before:-mx-64 before:-z-10 before:shadow">
         {children}
       </div>
     </div>
